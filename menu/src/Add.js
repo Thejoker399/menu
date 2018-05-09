4b801408e9aa8dbe2124b136abb873d47848f23d
thitaya <tsuaimak@gmail.com>
2018-05-09 18:02:20 +0700

    add-delete

--- menu/src/Add.js
+++ menu/src/Add.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react';
 class Add extends Component {
     state={
-        list:[''],
-        menu:''
+    //     list:[
+    //         { name: "ผัดไทย",price: "50 บาท" },
+    //         { name: "บะหมี่", price: "40 บาท" },
+    //         { name: "บะหมี่", price: "42 บาท" },
+    //         { name: "บะหมี่", price: "35 บาท" },
+    //         { name: "บะหมี่", price: "60 บาท" },
+    //         { name: "บะหมี่", price: "55 บาท" },
+    //         { name: "บะหมี่", price: "40 บาท" }
+    //     ],
+        name:"",
+        price:"",
     }
-    addlist=(event)=>{
+    // ChangeTodo = (event) => {
+    //     this.setState({ menu: event.target.value })
+    // }
+    addlist = (event) => {
         event.preventDefault()
-        const menu = this.state.menu
-        const lists = this.state.list
-        lists.push(menu)
-        this.setState({
-            list: lists ,
-            menu: ''
-        })
-        
+        if (this.state.name !== '' && this.state.price !== '') {
+
+            const name = this.state.name
+            const price = `${this.state.price} บาท`
+
+            this.props.addMenu(name, price)
+        }
     }
-    onChangeMenu=(event)=>{
-       this.setState({menu: event.target.value})
+
+    // addlistMenu =(event)=>{
+    //     event.preventDefault()
+    //     const menu = this.state.menu
+    //     if(menu){
+    //         this.props.addlist(menu)
+    //         this.setState({menu:''})
+    //     }  
+    // }
+    onChangeName = (event) => {
+        this.setState({ name: event.target.value })
+    }
+    onChangePrice = (event) => {
+        this.setState({ price: event.target.value })
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.addlist}>
-                <input type='text' value={this.state.menu} onChange={this.onChangeMenu}></input>
-                <button type='submit'>addlist</button>
+                    <input type='text' onChange={this.onChangeName} />
+                    <input type='number' min="1" onChange={this.onChangePrice} />
+                    <button type='submit'>addlist</button>
                 </form>
+
+
                 <ul>
+          
                     {
-                        this.state.list.map((value,index)=><li>{value}</li>)
+                    this.props.list.map((value, index) => <li key={`menu-${index}`}>{value.name}{value.price}ลบ</li>)
+
                     }
-                    </ul>
+              
+
+
+                </ul>
             </div>
         );
     }
--- menu/src/App.js
+++ menu/src/App.js
@@ -5,10 +5,39 @@ import './App.css';
 import Add from './Add'
 
 class App extends Component {
+  state = {
+    list: [
+      { name: "ผัดไทย", price: "50 บาท" },
+      { name: "บะหมี่", price: "40 บาท" },
+      { name: "บะหมี่", price: "42 บาท" },
+      { name: "บะหมี่", price: "35 บาท" },
+      { name: "บะหมี่", price: "60 บาท" },
+      { name: "บะหมี่", price: "55 บาท" },
+      { name: "บะหมี่", price: "40 บาท" }
+    ],
+  }
+  addMenu = (name, price) => {
+    // console.log(name)
+    if (name !== '' && price !== '') {
+      const menu = { name, price }
+      const list = this.state.list
+      list.push(menu)
+      this.setState({
+        list,
+      })
+    }
+
+  }
+
+  deleteMenu=(index) =>{
+    this.state.list.splice(index, 1);
+
+  }
+
   render() {
     return (
       <div>
-        <Add />
+        <Add addMenu={this.addMenu} list={this.state.list} />
       </div>
     );
   }
