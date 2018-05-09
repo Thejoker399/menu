8f1d5e4c2a66b509caf79379c92a7d80185c23a2
sirinrat <sirinratjanoon@outlook.com>
2018-05-09 18:02:48 +0700

    no message

--- menu/src/Header.js
+++ menu/src/Header.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-import { Cascader } from 'antd';
+import {Icon} from 'antd';
 
 
 export default class Header extends Component {
-    
-
-    onChange = (value, selectedOptions) => {
-        this.setState({
-          text: selectedOptions.map(o => o.label).join(', '),
-        })
-    }
 
     render() {
         return (
             <div className="header-top">
                 <span >
-                        <Cascader onChange={this.onChange}>
-                        <a className="span-cilck"> RECEPTIONIST</a>
-                    </Cascader>
+                        <a className="span-cilck"> <Icon type="tool" /> RECEPTIONIST</a>
                 </span>
             </div>
         )
