e132230e95383a43fafd1d4c0895010368e04453
sirinrat <sirinratjanoon@outlook.com>
2018-05-10 14:03:55 +0700

    no message

--- menu/src/Add.js
+++ menu/src/Add.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
+import { Modal, Button } from 'antd';
 class Add extends Component {
-    state={
+    state = {
 
-        name:"",
-        price:"",
+        name: "",
+        price: "",
     }
-        setStateNumberRandom=(evt)=>{
-            this.setState({
-                numberRandom: evt.target.value
-            })
-        }
-       randommenu=()=>{
-           
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+    handleOk = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+    handleCancel = (e) => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    }
+
+    setStateNumberRandom = (evt) => {
+        this.setState({
+            numberRandom: evt.target.value
+        })
+    }
+    randommenu = () => {
+
         const newlist = this.state.list
-        for(let i=0 ; i<this.state.numberRandom ; i++){
+        for (let i = 0; i < this.state.numberRandom; i++) {
             let randomlist = Math.floor(Math.random() * newlist.length);
             console.log(newlist[randomlist])
         }
-        
-        }  
-    
- 
+
+    }
+
+
     addlist = (event) => {
         event.preventDefault()
         if (this.state.name !== '' && this.state.price !== '') {
@@ -42,21 +62,30 @@ class Add extends Component {
     render() {
         return (
             <div>
+                <Button type="primary" onClick={this.showModal}>Open</Button>
+                <Modal
+                    title="Basic Modal"
+                    visible={this.state.visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+             
                 <form onSubmit={this.addlist}>
                     <input type='text' onChange={this.onChangeName} />
                     <input type='number' min="1" onChange={this.onChangePrice} />
                     <button type='submit'>addlist</button>
-                    
+
                 </form>
                 <ul>
                     {
-                    // this.props.list.map((value, index) => <li key={`menu-${index}`}> {value.name}{value.price} <button onClick={() =>this.props.deleteMenu(index)}> ลบ </button></li>)
+                        // this.props.list.map((value, index) => <li key={`menu-${index}`}> {value.name}{value.price} <button onClick={() =>this.props.deleteMenu(index)}> ลบ </button></li>)
 
                     }
                 </ul>
-                
-                <button onClick={this.clearAll}>clear</button>   
-               
+                </Modal>
+                <div className="block">
+                <button onClick={this.clearAll}>clear</button>
+                </div>
             </div>
         );
     }
