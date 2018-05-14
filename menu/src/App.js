e2430465588e55f7e5428b901d95615229fe672b
sirinrat <sirinratjanoon@outlook.com>
2018-05-14 17:50:40 +0700

    no message

--- menu/src/App.js
+++ menu/src/App.js
@@ -57,7 +57,7 @@ class App extends Component {
     });
   }
 
-  deleteMenu = (value,index) => {
+  deleteMenu = (index,value) => {
     // console.log('index', index)
     const arr = [...this.state.lists]
     // console.log(arr)
@@ -112,9 +112,8 @@ class App extends Component {
       let list = []
       list.push(menu)
       this.setState({
-        lists: this.state.lists.concat(list),       
+        lists: this.state.lists.concat(list),      
       })
-
     }
   }
 
